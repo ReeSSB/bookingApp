989513ef9907582ec23281d8f7400294cea2b743
srshashibhushan <Shashi.as05@gmail.com>
2023-09-30 16:33:02 +0530

    Update Login.jsx

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -6,8 +6,8 @@ import "./login.css";
 
 const Login = () => {
 	const [credentials, setCredentials] = useState({
-		username: undefined,
-		password: undefined,
+		username:username,
+		password:password,
 	});
 
 	const { loading, error, dispatch } = useContext(AuthContext);
